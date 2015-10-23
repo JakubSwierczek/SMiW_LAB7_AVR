a60ab6e35728c1be1980070e8db3c412ab7393a4
Jakub Swierczek <jakub.swierczek@outlook.com>
2015-10-23 23:56:19 +0200

    Zrobione C
    Wszystko w C napisane, zbudowane, nie przetestowane bo debugger nie chce
    zlapac breakpointa, nie mam pojecia czemu, ale chyba wszystko napisane
    tak jak powinno byc

--- ProgramC/src/main.c
+++ ProgramC/src/main.c
@@ -14,49 +14,73 @@
 ; Version: 1.0
 ;//////////////////////////////////////////////////////////////////////////////
 */
+#define F_CPU 16000000L
 #include <avr/io.h>
+#include <avr/eeprom.h>
 #include <avr/pgmspace.h>
 
-#define nLength 100
-/*
-uint8_t TAB_ROM[] PROGMEM = { 0x00, 0x00 };
-uint8_t TAB_RAM[nLength];
-*/
-// uncomment if needed
-// #define GET_FAR_ADDRESS(var) \
-//({ \
-//uint_farptr_t tmp; \
-//__asm__ __volatile__( \
-//"ldi %A0, lo8(%1)" "\n\t" \
-//"ldi %B0, hi8(%1)" "\n\t" \
-//"ldi %C0, hh8(%1)" "\n\t" \
-//"clr %D0" "\n\t" \
-//: \
-//"=d" (tmp) \
-//: \
-//"p" (&(var)) \
-//); \
-//tmp; \
-//})
-
-//---------------------------------------------------------------------
-void main (void)
+#define GET_FAR_ADDRESS(var) \
+({ \
+	uint_farptr_t tmp; \
+	\
+	__asm__ __volatile__( \
+	\
+	"ldi %A0, lo8(%1)" "\n\t" \
+	"ldi %B0, hi8(%1)" "\n\t" \
+	"ldi %C0, hh8(%1)" "\n\t" \
+	"clr %D0" "\n\t" \
+	: \
+	"=d" (tmp) \
+	: \
+	"p" (&(var)) \
+	); \
+	tmp; \
+})
+void wypisz_dane(uint8_t dana);
+uint8_t czytaj_pamiec(const int offset, uint8_t* poprzednia_wartosc, uint8_t* obecna_wartosc);
+const uint8_t PROGMEM romtab[10] = {0xF0, 0x10, 0xFF, 0x11, 0x00, 0x01, 0xFA, 0x23, 0x00, 0x00};
+	
+	
+uint8_t czytaj_pamiec(const int offset, uint8_t* poprzednia_wartosc, uint8_t* obecna_wartosc)
 {
-	//---------------------------------------------------------------------
-	// Main program code place here
-	// 1. Place here code related to initialization of ports and interrupts
-	// for instancje port A as output and initial value 0
-	// DDRA=0xFF
-	// PORTA=0x00
-
-	// 2. Enable interrupts if needed
-	// sei();
-
-	// 3. Place here main code
-
-
-	//----------------------------------------------------------------------
-	// Program end
-	//----------------------------------------------------------------------
+	*poprzednia_wartosc = *obecna_wartosc;
+	*obecna_wartosc = romtab[offset];
+	if(*obecna_wartosc == *poprzednia_wartosc && *obecna_wartosc == 0)
+		return 0x00;
+	return 0xFF;
+	
 }
-// -------------------------------------------------------------------
+
+void wypisz_dane(uint8_t dana)
+{
+	PORTB = dana;
+}
+int main(void)
+{
+	int offset = 0;
+	uint8_t czy_wpisac = 0x00;
+	uint8_t poprzednia_wartosc = 0x00;
+	uint8_t obecna_wartosc = 0xFF;
+	DDRA = 0x00; // wejscie
+	DDRB = 0x00; // wyjscie
+	PORTA = 0xFF; // + pullup-y
+	PINA = 0xFF;
+	PORTB = 0x00;
+	PINB = 0x00;
+	
+	while(1)
+	{
+		if(PINA != 0xFF)
+		{
+			czy_wpisac = czytaj_pamiec(offset, &poprzednia_wartosc, &obecna_wartosc);
+			if(czy_wpisac == 0xFF)
+				wypisz_dane(obecna_wartosc);
+			else
+				break;
+		}	
+	}
+	
+	
+	return 0;
+}
+	
